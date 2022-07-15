ec7c49a8cd8df10b6aa504e2cb13f238ca9d4424
Ben Goodrow <bpgoodrow@gmail.com>
2022-07-15 09:08:50 -0700

    trying to figure out tests WIP

--- Bakery/Models/Bakery.cs
+++ Bakery/Models/Bakery.cs
@@ -2,8 +2,39 @@ using System;
 
 namespace Bakery.Models
 {
-  public class Coffee
+  public class Bakery
   {
-    
+    private static double Price = 5;
+    public int Quantity { get; }
+
+    public static double Price
+    {
+      get { return _price; }
+    }
+
+    public Bread(int quantity)
+    {
+      _price = Price;
+      Quantity = quantity;
+    }
+
+    public double breadCost()
+    {
+      double breadTotal = 0;
+      for (int index = 0; index <= Quantity; index++)
+      {
+        if (index == 0)
+        {
+          breadTotal += 0;
+        }
+        else
+        {
+          {
+            breadTotal =+ _price;
+          }
+        }
+      }
+      return breadTotal;
+    }
   }
 }
