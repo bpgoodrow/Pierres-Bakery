5c2e62cd86e1d77c8e5ff5cb20d5049e1f034f08
Ben Goodrow <bpgoodrow@gmail.com>
2022-07-15 11:09:47 -0700

    Create new test for pastry, begin creating pastry class and UI

--- Bakery.Tests/ModelTests/BakeryTests.cs
+++ Bakery.Tests/ModelTests/BakeryTests.cs
@@ -17,4 +17,16 @@ namespace Bakery.Tests
       Assert.AreEqual(breadCost, breadOrder.breadCost());
     }
   }
+  [TestClass]
+  public class PastryTests
+  {
+    [Test Method]
+    public void PastryCost_CostOnePastry_Int();
+    {
+      int pastryQuantity = 1;
+      double pastryCost = 2;
+      Pastry pastryOrder = new Pastry(pastryQuantity);
+      Assert.AreEqual(pastryCost, pastryOrder.PastryCost());
+    }
+  }
 }
